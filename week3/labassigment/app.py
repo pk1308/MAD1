abc5e95dce8060b9bbf50bb46fcbc949788ad474
pk1308 <princevkurien@gmail.com>
2024-06-17 17:12:29 +0400

    update formatted

--- week3/labassigment/app.py
+++ week3/labassigment/app.py
@@ -1,22 +1,23 @@
 from jinja2 import Template
-import matplotlib.pyplot as plt 
-import sys 
-import csv 
-import os 
+import matplotlib.pyplot as plt
+import sys
+import csv
+import os
+
 
 class Lab_assignment:
-    def __init__(self , data_path = 'data.csv') -> None:
+    def __init__(self, data_path="data.csv") -> None:
         self.file_name = data_path
-        
-    def render_save_file(self, data : dict , template : Template ):
+
+    def render_save_file(self, data: dict, template: Template):
         rendered_html = template.render(data=data)
 
-    # Open a file for writing with UTF-8 encoding
-        with open('output.html', 'w', encoding='utf-8') as my_html_document_file:
+        # Open a file for writing with UTF-8 encoding
+        with open("output.html", "w", encoding="utf-8") as my_html_document_file:
             # Write the rendered HTML content to the file
             my_html_document_file.write(rendered_html)
-    
-    def plot_save(self,data : dict ):
+
+    def plot_save(self, data: dict):
         plt.hist(data)
         plt.xlabel("Marks")
         plt.ylabel("Frequency")
@@ -25,32 +26,31 @@ class Lab_assignment:
         plt.savefig(file_path)
         plt.close()
         return file_path
-        
-        
-    def get_data(self , student_id = None  , course_id = None):
+
+    def get_data(self, student_id=None, course_id=None):
         student_data = []
         course_data = {}
-        file_path = os.path.join( os.getcwd(), self.file_name)
-        with open(file_path, 'r') as file:
+        file_path = os.path.join(os.getcwd(), self.file_name)
+        with open(file_path, "r") as file:
             reader = csv.reader(file)
             header = [j.strip() for j in next(reader)]
             if student_id is not None:
                 for row in reader:
                     if int(row[0]) == int(student_id):
-                        student_data.append(dict(zip(header, [int(i) for i in row])) )
+                        student_data.append(dict(zip(header, [int(i) for i in row])))
                 return student_data
             elif student_id is None:
                 data = []
                 for row in reader:
                     if int(row[1]) == int(course_id):
                         data.append(int(row[2]))
-                course_data["average_marks"] =  sum(data) / len(data)
+                course_data["average_marks"] = sum(data) / len(data)
                 course_data["max"] = max(data)
-                return course_data , data 
+                return course_data, data
             else:
-                pass 
-                
-    def get_template(self, ):
+                pass
+
+    def get_template( self,):
         TEMPLATE = """<!doctype html>
             <html>
             <head>
@@ -110,34 +110,40 @@ class Lab_assignment:
             """
         template = Template(TEMPLATE)
         return template
-    def run_script(self,):
+
+    def run_script( self, ):
         data_to_render = {}
         template = self.get_template()
-        c_index = sys.argv.index('-c') if '-c' in sys.argv else -1
-        s_index = sys.argv.index('-s') if '-s' in sys.argv else -1
-        # check order 
-        if s_index==1 and c_index== -1: 
-            student_id = sys.argv[s_index+1]
+        c_index = sys.argv.index("-c") if "-c" in sys.argv else -1
+        s_index = sys.argv.index("-s") if "-s" in sys.argv else -1
+        # check order
+        if s_index == 1 and c_index == -1:
+            student_id = sys.argv[s_index + 1]
             data_to_render["title"] = "Student Data"
             data_to_render["heading"] = "Student Details"
             data_to_render["student_data"] = self.get_data(student_id=student_id)
-            data_to_render["total_marks"]= sum([d.get("Marks") for d in data_to_render["student_data"] ])
+            data_to_render["total_marks"] = sum(
+                [d.get("Marks") for d in data_to_render["student_data"]]
+            )
             self.render_save_file(data=data_to_render, template=template)
-        elif c_index ==1 and s_index== -1:
-            course_id= sys.argv[c_index+1]
+        elif c_index == 1 and s_index == -1:
+            course_id = sys.argv[c_index + 1]
             data_to_render["title"] = "Course Data"
             data_to_render["heading"] = "Course Details"
-            data_to_render["course_data"] , data_to_plot = self.get_data(course_id=course_id)
-            
+            data_to_render["course_data"], data_to_plot = self.get_data(
+                course_id=course_id
+            )
+
             data_to_render["plot_path"] = self.plot_save(data=data_to_plot)
             self.render_save_file(data=data_to_render, template=template)
         else:
-            
+
             data_to_render["title"] = "Something Went Wrong"
             data_to_render["heading"] = "Wrong Input"
             data_to_render["wrong_data"] = "Something Went Wrong"
             self.render_save_file(data=data_to_render, template=template)
-    
+
+
 if __name__ == "__main__":
     lab = Lab_assignment()
     lab.run_script()
